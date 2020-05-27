389ad3c6cda517c39f8fbd44ff44e206f0f59e83
masumcsepust <masumcsepust@gmail.com>
2020-05-27 12:38:43 +0600

    with all files

--- movie/settings.py
+++ movie/settings.py
@@ -11,22 +11,22 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from decouple import config
+from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
+STATIS_ROOT = os.path.join(BASE_DIR, 'statisfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-$=^7_j5fkx+wdx6p5-^zls5c&4i7w0c)+qv291k0^()jb)3vs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'mrmasum.herokuapp.com'
+]
+SECRET_KEY=config('SECRET_KEY')
+DEBUG=config('DEBUG',default=False,cast=bool)
 
 # Application definition
 
@@ -80,13 +80,10 @@ WSGI_APPLICATION = 'movie.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+default_dburl='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
 
 REST_FRAMEWORK={
     'DEFAULT_PERMISSION_CLASSES':{
